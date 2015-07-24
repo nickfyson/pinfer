030b18306f042f9eb45217b4c0d3b48bd61186ec
Nick Fyson <mail@nickfyson.co.uk>
2015-07-24 11:56:09 +0100

    Introduce tree visualisation function

--- pinfer/__init__.py
+++ pinfer/__init__.py
@@ -8,3 +8,4 @@ __version__ = '0.1.0'
 from .pinfer import analyse_tree  # NOQA
 from .io import load_notung_nhx  # NOQA
 from .itree import build_itree  # NOQA
+from .visualise import vis_tree  # NOQA
---
+++ pinfer/visualise.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""module docstring here"""
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import networkx as nx
+
+
+def vis_tree(tree, fig=None, pos=None, node_cat_string=None, pallete_name='rainbow', node_size=None):
+    """visualise the tree with colouring according to property 'node_cat_string'"""
+    
+    if not fig:
+        plt.figure(figsize=(20, 6))
+    
+    if not pos:
+        pos = nx.graphviz_layout(tree, prog='dot')
+
+    node_colors = 'b'
+    
+    if node_cat_string:
+
+        all_categories = set([tree.node[n][node_cat_string] for n in tree.nodes()])
+
+        all_categories = sorted(list(all_categories))
+
+        pallette = sns.color_palette(pallete_name, len(all_categories))
+
+        color_dict = {cat: pallette[i] for i, cat in enumerate(all_categories)}
+
+        node_colors = []
+        for node in tree.nodes():
+        
+            category = tree.node[node][node_cat_string]
+
+            node_colors.append(color_dict[category])
+    
+    if not node_size:
+        node_size = float(6e4) / len(tree.nodes())
+
+    if node_size > 2000:
+        with_labels = True
+        font_size   = node_size / 400.0
+    else:
+        with_labels = False
+        font_size   = node_size / 400.0
+
+    nx.draw(tree, pos, arrows=False,
+            with_labels=with_labels, font_size=font_size,
+            node_color=node_colors,
+            linewidths=0.0, node_size=node_size)
