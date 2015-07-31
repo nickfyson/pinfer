85cb3359f499a981a962eb39bc98b5060e7558b8
Nick Fyson <mail@nickfyson.co.uk>
2015-07-31 17:08:43 +0100

    Introduce prototype PyMC modelling

--- pinfer/__init__.py
+++ pinfer/__init__.py
@@ -6,6 +6,7 @@ __version__ = '0.1.0'
 
 
 from .pinfer import analyse_tree  # NOQA
+from .pinfer import analyse_pymc  # NOQA
 from .io import load_notung_nhx  # NOQA
 from .itree import build_itree  # NOQA
 from .visualise import vis_tree  # NOQA
--- pinfer/pinfer.py
+++ pinfer/pinfer.py
@@ -8,7 +8,7 @@ import networkx as nx
 def analyse_tree(tree, update_repeats=20, verbose=True):
     """perform VB inference on networkx representation of interaction tree"""
 
-    from bayespy.nodes import Categorical, Mixture
+    from bayespy.nodes import Categorical, Mixture, Gate
 
     variables = {}
 
@@ -40,3 +40,47 @@ def analyse_tree(tree, update_repeats=20, verbose=True):
 
     for node, variable in variables.items():
         tree.node[node]['posterior'] = variable.pdf(1)
+
+
+def analyse_pymc(tree, samples=1000, burns=500):
+    """docstring for analyse_pymc"""
+    
+    import numpy as np
+    import pymc as pm
+
+    variables = {}
+
+    for node in nx.topological_sort(tree):
+
+        # if there are no incoming edges, this node is the initial state
+        if tree.in_degree(node) == 0:
+            prior = tree.node[node]['prior']
+            
+            variables[node] = pm.Categorical(node, [1 - prior, prior])
+
+        for s, t in tree.in_edges(node):
+
+            p_transitions = tree.edge[s][t]['p_transitions']
+            
+            variables['t_%s' % t] = pm.Index('t_%s' % t, p_transitions, variables[s])
+            
+            if 'observed' in tree.node[t]:
+                observed = True
+                value    = int(tree.node[t]['observed'])
+            else:
+                observed = None
+                value    = None
+            
+            variables[t] = pm.Categorical(t, variables['t_%s' % t],
+                                          observed=observed, value=value)
+    
+    model = pm.MCMC(list(variables.values()))
+
+    model.sample(10000, 5000)
+
+    for node in tree.nodes():
+        tree.node[node]['posterior'] = np.mean(model.trace(node)[:])
+
+    model = None
+
+    return tree
