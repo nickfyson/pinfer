8176abaf4dcc206a6ae1c9f58ed21bc88acd2510
Nick Fyson <mail@nickfyson.co.uk>
2015-09-15 14:47:45 +0100

    Add unit test for polytree algorithm

--- tests/test_pinfer.py
+++ tests/test_pinfer.py
@@ -16,31 +16,64 @@ import networkx as nx
 import numpy as np
 
 
-class TestPinfer(unittest.TestCase):
+class TestPolytree(unittest.TestCase):
 
     def setUp(self):
-        self.tree = nx.DiGraph(name='interaction tree')
 
-        self.tree.add_node(0, prior=0.5)
-        self.tree.add_node(1, prior=0.5)
-        self.tree.add_node(2, prior=0.5)
+        tree = nx.DiGraph()
 
-        self.tree.add_edge(0, 1,
-                           p_transitions=np.array([[0.5, 0.5], [0.1, 0.9]]))
-        self.tree.add_edge(0, 2,
-                           p_transitions=np.array([[0.5, 0.5], [0.2, 0.8]]))
+        tree.add_node('R', prior=np.array([0.8, 0.2]))
+        tree.add_node('S', prior=np.array([0.9, 0.1]))
 
-    def test_full_inference(self):
-        self.tree.node[1]['observed'] = 0
+        CPT_W = np.zeros((2, 2))
+        CPT_W[0, :] = np.array([0.8, 0.2])
+        CPT_W[1, :] = np.array([0.0, 1.0])
 
-        pinfer.analyse_tree(self.tree, verbose=False)
+        tree.add_node('W', CPT=CPT_W, diagnostic=np.array([1, 1]))
 
-        assert round(self.tree.node[0]['posterior'], 12) == 0.166666666667
-        assert round(self.tree.node[1]['posterior'], 12) == 0.75
-        assert round(self.tree.node[2]['posterior'], 12) == 0.557506665976
+        CPT_H = np.zeros((2, 2, 2))
+        CPT_H[0, 0, :] = np.array([1.0, 0.0])
+        CPT_H[0, 1, :] = np.array([0.1, 0.9])
+        CPT_H[1, 0, :] = np.array([0.0, 1.0])
+        CPT_H[1, 1, :] = np.array([0.0, 1.0])
 
-        self.tree.node[1].pop('observed')
+        tree.add_node('H', CPT=CPT_H, diagnostic=np.array([1, 1]))
 
+        tree.add_edge('R', 'W')
+        tree.add_edge('R', 'H')
+        tree.add_edge('S', 'H')
+
+        self.tree = tree
+
+    def test_polytree(self):
+
+        from pinfer.pinfer import polytree
+
+        polytree(self.tree)
+
+        assert (np.round(self.tree.node['R']['belief'], 8) == np.array([0.8, 0.2])).all()
+        assert (np.round(self.tree.node['S']['belief'], 8) == np.array([0.9, 0.1])).all()
+        assert (np.round(self.tree.node['W']['belief'], 8) == np.array([0.64, 0.36])).all()
+        assert (np.round(self.tree.node['H']['belief'], 8) == np.array([0.728, 0.272])).all()
+
+        self.tree.node['H']['observation'] = np.array([0., 1.])
+
+        polytree(self.tree)
+
+        assert (np.round(self.tree.node['R']['belief'], 8) == np.array([0.26470588, 0.73529412])).all()
+        assert (np.round(self.tree.node['S']['belief'], 8) == np.array([0.66176471, 0.33823529])).all()
+        assert (np.round(self.tree.node['W']['belief'], 8) == np.array([0.21176471, 0.78823529])).all()
+        assert (np.round(self.tree.node['H']['belief'], 8) == np.array([0.00000000, 1.00000000])).all()
+        
+        self.tree.node['W']['observation'] = np.array([0., 1.])
+
+        polytree(self.tree)
+
+        assert (np.round(self.tree.node['R']['belief'], 8) == np.array([0.06716418, 0.93283582])).all()
+        assert (np.round(self.tree.node['S']['belief'], 8) == np.array([0.83955224, 0.16044776])).all()
+        assert (np.round(self.tree.node['W']['belief'], 8) == np.array([0.00000000, 1.00000000])).all()
+        assert (np.round(self.tree.node['H']['belief'], 8) == np.array([0.00000000, 1.00000000])).all()
+        
     def tearDown(self):
         pass
 
