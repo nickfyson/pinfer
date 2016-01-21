046812bbf2fde669f5ce0da8d6ed9e585e312fb9
Nick Fyson <mail@nickfyson.co.uk>
2016-01-21 15:31:37 +0000

    Rename function for clarity and add illuminating comments
    Also – whitespace corrections

--- pinfer/itree.py
+++ pinfer/itree.py
@@ -35,44 +35,44 @@ def add_normalised_edge_lengths(tree):
     """annotate all edges such that total depth of tree is normalised to one within each species"""
 
     def relabel_weights(tree, effective_root, remaining_length=1.0):
-        
+
         for child in tree.edge[effective_root]:
-            
+
             max_dist = tree.edge[effective_root][child]['weight'] + \
                 max([
                     d for n, d in
                     nx.shortest_path_length(tree, source=child, weight='weight').items()
                 ])
-            
+
             original_length = tree.edge[effective_root][child]['weight']
-            
+
             new_length      = remaining_length * (original_length / max_dist)
-            
+
             # label outgoing edges with the appropriate fraction of the remaining length
             tree.edge[effective_root][child]['length'] = new_length
-            
+
             # call relabel_weights with each child as effective root, and correct remaining length
             relabel_weights(tree, child, remaining_length=(remaining_length - new_length))
-            
+
         return
 
     all_species = set([tree.node[n]['S'] for n in tree.nodes()])
 
     for i, species in enumerate(all_species):
-        
+
         nodes = [n for n in tree.nodes() if tree.node[n]['S'] == species]
-        
+
         # add parents to the set of nodes
         parents = set()
         for node in nodes:
             if tree.predecessors(node):
                 parents.add(tree.predecessors(node)[0])
-        
+
         subTree = nx.subgraph(tree, nodes + list(parents))
-        
+
         for root in [n for n in subTree.nodes() if not subTree.predecessors(n)]:
             relabel_weights(subTree, root)
-        
+
         for s, t in subTree.edges():
             tree.edge[s][t]['length'] = subTree.edge[s][t]['length']
 
@@ -83,7 +83,7 @@ def label_death_times(tree):
     """add birth and death time properties to each node"""
 
     add_normalised_edge_lengths(tree)
-    
+
     root = [n for n in tree.nodes() if not tree.predecessors(n)][0]
 
     for n, d in nx.shortest_path_length(tree, root, weight='length').items():
@@ -94,20 +94,20 @@ def label_death_times(tree):
             tree.node[n]['t_birth'] = tree.node[tree.predecessors(n)[0]]['t_death']
         except IndexError:
             tree.node[n]['t_birth'] = 0.0
-    
-    return
-    
 
-def get_extants(iTree, gene):
+    return
+
+
+def get_fellow_extants(iTree, gene):
     """returns a list of all interaction partners of the specified gene"""
 
     if _is_lost(iTree, gene):
         return []
 
     time = iTree.node[gene]['t_birth']
-    
+
     extants = []
-    
+
     for n in iTree.nodes():
         if iTree.node[n]['node_type'] != 'gene':
             pass
@@ -127,15 +127,17 @@ def get_extants(iTree, gene):
 
 def get_parent_interaction(iTree, interaction):
     """return the appropriate parent for the given interaction"""
-    
-    # geneA, geneB = interaction.split('_')
+
+    # the parent gene(s) are attached as predecessors in the graph
+
     predecessors = iTree.predecessors(interaction)
+
     geneA = predecessors[0]
     try:
         geneB = predecessors[1]
-    except IndexError:
+    except IndexError:  # if there isn't a 2nd predecessor, we know it's a self interaction
         geneB = predecessors[0]
-    
+
     try:
         if iTree.node[geneA]['t_birth'] > iTree.node[geneB]['t_birth']:
             ancestorA = iTree.predecessors(geneA)[0]
@@ -146,7 +148,7 @@ def get_parent_interaction(iTree, interaction):
     except IndexError:
         # if there are no ancestors, then we're looking at the ancenstral interaction
         return None, None
-    
+
     while True:
 
         # if we have found the same ancestor for both, we know that
@@ -160,7 +162,7 @@ def get_parent_interaction(iTree, interaction):
                          if iTree.node[n]['node_type'] == 'interaction'])
         childrenB = set([n for n in iTree.successors(ancestorB)
                          if iTree.node[n]['node_type'] == 'interaction'])
-        
+
         # as a sanity check, it should be impossible to have *more* than 1 common child
         if len(childrenA.intersection(childrenB)) > 1:
             raise Exception('Uh oh! Too many children in common!')
@@ -168,7 +170,7 @@ def get_parent_interaction(iTree, interaction):
         if childrenA.intersection(childrenB):
             parent_interaction = childrenA.intersection(childrenB).pop()
             break
-        
+
         # if not, we replace the most recently deceased ancestor node with *its* ancestor gene
         if iTree.node[ancestorA]['t_birth'] > iTree.node[ancestorB]['t_birth']:
             ancestorA = iTree.predecessors(ancestorA)[0]
@@ -176,14 +178,14 @@ def get_parent_interaction(iTree, interaction):
         else:
             ancestorA = ancestorA
             ancestorB = iTree.predecessors(ancestorB)[0]
-    
+
     # we can also return the edge length here...
     evolution_of_A = nx.shortest_path_length(iTree, source=ancestorA, target=geneA, weight='weight')
     evolution_of_B = nx.shortest_path_length(iTree, source=ancestorB, target=geneB, weight='weight')
     evol_dist    = evolution_of_A + evolution_of_B
 
     return parent_interaction, evol_dist
-    
+
 
 def _is_lost(iTree, gene):
     """simple function to determine whether the gene has been lost"""
@@ -191,35 +193,34 @@ def _is_lost(iTree, gene):
 
 
 def build_itree(gTree):
-    """function to construction interaction tree, given suitably annotated gene tree"""
+    """function to construct interaction tree, given suitably annotated gene tree"""
     # we initialise as a copy of the gTree, so the new tree can be built on the existing structure
-    
+
     iTree = deepcopy(gTree)
     iTree.graph['name'] = 'iTree'
 
     # all existing nodes are annotated as genes
     for node in iTree.nodes():
         iTree.node[node]['node_type'] = 'gene'
- 
+
     label_death_times(iTree)
 
     # first we build a list of nodes ordered by the distance from the root
     # ordered_nodes = get_ordered_nodes(iTree)
-    
-    ordered_nodes = [(n, iTree.node[n]['t_birth']) for n in iTree.nodes()]
 
+    ordered_nodes = [(n, iTree.node[n]['t_birth']) for n in iTree.nodes()]
     ordered_nodes = [n for n, d in sorted(ordered_nodes, key=lambda x:x[1])]
 
     for gene in ordered_nodes:
 
-        for extant in get_extants(iTree, gene):
-            
+        for fellow in get_fellow_extants(iTree, gene):
+
             # if doesn't already exist, add an interaction between node and gene
-            if get_inode_name(gene, extant) not in iTree.nodes():
-                
-                new_interaction = get_inode_name(gene, extant)
+            if get_inode_name(gene, fellow) not in iTree.nodes():
+
+                new_interaction = get_inode_name(gene, fellow)
                 new_int_name    = get_inode_name(gTree.node[gene]['name'],
-                                                 gTree.node[extant]['name'])
+                                                 gTree.node[fellow]['name'])
                 iTree.add_node(new_interaction,
                                node_type='interaction',
                                S=iTree.node[gene]['S'],
@@ -227,25 +228,32 @@ def build_itree(gTree):
                                )
 
                 iTree.add_edge(gene, new_interaction)
-                iTree.add_edge(extant, new_interaction)
+                iTree.add_edge(fellow, new_interaction)
 
                 parent_interaction, evol_dist = get_parent_interaction(iTree, new_interaction)
 
                 if parent_interaction:
                     iTree.add_edge(parent_interaction, new_interaction, evol_dist=evol_dist)
-    
+
     # we can use the remaining gTree nodes to mark all the extant interactions
+    # we first list all the extant gene nodes in the present time - ie. no children
     extant_gnodes = set([n for n in gTree.nodes() if not gTree.successors(n)])
 
     for inode in [n for n in iTree.nodes() if iTree.node[n]['node_type'] == 'interaction']:
 
         parent_genes = [n for n in iTree.predecessors(inode)
                         if iTree.node[n]['node_type'] == 'gene']
-               
+        # if the parent gene(s) is / are both extant, then so is the interaction
         if set(parent_genes).issubset(extant_gnodes):
             iTree.node[inode]['extant'] = True
-    
+
     # we don't want the gTree nodes actually remaining as part of the iTree
     iTree.remove_nodes_from([n for n in iTree.nodes() if iTree.node[n]['node_type'] == 'gene'])
-    
+
+    # instead of inscrutable numbers as the nodes,
+    # we relabel using the 'name' property
+    nx.relabel_nodes(iTree,
+                     {n: iTree.node[n]['name'] for n in iTree.nodes()},
+                     copy=False)
+
     return iTree
