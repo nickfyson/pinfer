8328fe9db913b75ea6e3b5254c97298df99269a6
Nick Fyson <mail@nickfyson.co.uk>
2016-01-28 16:20:31 +0000

    Introduce original_build_itree function to pinfer.itree module
    Permits calling of original iTree construction code. Comparison could be instructive,
    not least to understand which is more correct!

--- pinfer/itree/__init__.py
+++ pinfer/itree/__init__.py
@@ -15,3 +15,18 @@ def build_itree(gTree):
     add_all_inodes(iTree)
 
     return iTree
+
+
+from .interact_original import add_all_inodes as original_add_all_inodes
+
+
+def original_build_itree(gTree):
+    """function to construct interaction tree, given suitably annotated gene tree"""
+
+    iTree = initialise_iTree(gTree)
+
+    label_birth_death(iTree)
+
+    original_add_all_inodes(iTree)
+
+    return iTree
