3989c324a76a5a25c9bdc86156cca9bb2edf56ca
Nick Fyson <mail@nickfyson.co.uk>
2016-02-10 15:51:37 +0000

    Move some CPT code inside the annotate submodule

--- pinfer/itree/annotate.py
+++ pinfer/itree/annotate.py
@@ -1,5 +1,6 @@
 
 import networkx as nx
+import numpy as np
 
 
 def annotate_observations(iTree, observations, only_leaves=True):
@@ -16,8 +17,22 @@ def annotate_observations(iTree, observations, only_leaves=True):
 
         observation = observations.get(node, None)
 
-        if observation:
-            iTree.node[node]['observation'] = observation
+        if not observation:
+            continue
+
+        new_node = 'observation_' + node
+
+        CPT = np.zeros((2, 2))
+        CPT[0, :] = np.array([1.0, 0.0])
+        CPT[1, :] = np.array([0.0, 1.0])
+
+        iTree.add_node(new_node,
+                       name=new_node,
+                       observation=observation,
+                       CPT=CPT,
+                       node_type='observation')
+
+        iTree.add_edge(node, new_node)
 
     # we assume a 50% chance of the ancestral self-interaction existing
     iTree.node[nx.topological_sort(iTree)[0]]['prior'] = [0.5, 0.5]
