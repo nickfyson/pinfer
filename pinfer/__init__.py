07e0200351959a1c5522b5e868d1886ce5d0b182
Nick Fyson <nick@fyson.net>
2016-02-22 14:35:20 +0000

    Add bayesian-belief-networks as git submodule
    Protect URL for submodule is... https://github.com/nickfyson/bayesian-belief-networks

--- pinfer/__init__.py
+++ pinfer/__init__.py
@@ -4,6 +4,11 @@ __author__ = 'Nick Fyson'
 __email__ = 'mail@nickfyson.co.uk'
 __version__ = '0.1.0'
 
+# we add the bbn module to the runtime path
+import sys, os
+sys.path.insert(1, os.path.abspath('bayesian-belief-networks'))
+# added at the start of the path to ensure we supercede any existing installation
+# not added to the *very* start as that can cause problems...
 
 from .io import load_notung_nhx  # NOQA
 from .itree import build_itree  # NOQA
