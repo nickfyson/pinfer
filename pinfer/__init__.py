fd9001a1226d9b12daca701e0318d04f025ac3eb
Nick Fyson <nick@fyson.net>
2016-02-22 17:17:01 +0000

    Fix import of bayesian-belief-networks submodule

--- pinfer/__init__.py
+++ pinfer/__init__.py
@@ -5,10 +5,14 @@ __email__ = 'mail@nickfyson.co.uk'
 __version__ = '0.1.0'
 
 # we add the bbn module to the runtime path
-import sys, os
-sys.path.insert(1, os.path.abspath('bayesian-belief-networks'))
+import sys
+import os
+path_comps = [os.path.dirname(os.path.abspath(__file__)), '..', 'bayesian-belief-networks']
+sys.path.insert(1, os.path.sep.join(path_comps))
 # added at the start of the path to ensure we supercede any existing installation
 # not added to the *very* start as that can cause problems...
+print(os.path.sep.join(path_comps))
+import bayesian  # NOQA
 
 from .io import load_notung_nhx  # NOQA
 from .itree import build_itree  # NOQA
