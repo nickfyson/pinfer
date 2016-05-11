56813055d6469cd7f5d21f99bd3ef077d22c056b
Nick Fyson <nick@fyson.net>
2016-05-11 17:28:06 +0100

    REL: bump the version number to 0.6.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="pinfer",
-    version="0.6",
+    version="0.6.1",
     author="Nick Fyson",
     author_email="mail@nickfyson.co.uk",
     description="Inference of ancestral Protein Interaction Networks (PINs).",
